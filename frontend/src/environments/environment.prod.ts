195165b3484ea643ebcf80dc0354e436efbb443f
sadamvenugopal <sadam.venugopal@gmail.com>
2025-04-21 16:55:21 +0530

    prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backendUrl: 'http://flooringproduction:3000' // replace with your backend container name
+    backendUrl: 'http://oows4c4osocgosc4oo0s8s8o.82.25.109.195.sslip.io' // replace with your backend container name
 };
   
